9a285e5559dcb2dd9907b552cde961626608351b
Tom Cowell <tc@eightsquarestudio.com>
2010-03-28 00:52:12 +0000

    cleaning up animation to be a tidier Class with the this[0] = elem, instead of this._elem = elem, adding reverse functionality to animate an animation back to the start position

--- src/animation.js
+++ src/animation.js
@@ -1,87 +1,126 @@
 var FRAME_RATE = 24,
-	TEST_TYPE = /padding|margin|height|width|top|left|right|bottom/;
+	TEST_TYPE = /padding|margin|height|width|top|left|right|bottom/;   
+	
+function init( opts ){
+
+	this._interval = Math.round(1000/ ( opts.frameRate || FRAME_RATE ) );
+	this._frames = Math.round( ( opts.duration || 600 ) * ( ( opts.frameRate || FRAME_RATE ) / 1000 ) );   
+	this._start = {};
+	this._delta = {};
+	
+	// check for supported css animated features and prep for animation
+	for( var key in opts.setStyle ){
+		
+		var opacity = ( key === 'opacity' && opts.setStyle[ key ] >= 0 && opts.setStyle[ key ] <= 1 );
+		
+		if( opacity || TEST_TYPE.test( key ) ){
+
+			this._appendString = opacity ? '' : 'px';
+
+			var start = ( currentCSS( this[0], key ) + '' || '0').replace('px','') * 1;
+
+			this._start[ key ] = start;				
+			this._delta[ key ] = ( (opts.setStyle[ key ] + '' || '0').replace('px','') * 1 ) - start;
+		}
+	}
+	
+	this._callback = ( typeof opts.callback === 'function' ) ? opts.callback : opts.reverse ? Simples.setContext( this, this.reverse ) : null;
+
+	this._autoStart = opts.manualStart !== true;  
+	debugger;
+	
+	return this._autoStart ? this.start() : this;	
+}         
+
+function tween( frame, frameCount, start, delta) {
+	return ((frame /= frameCount / 2) < 1) ? delta / 2 * frame * frame + start : -delta / 2 * ((--frame) * (frame - 2) - 1) + start;
+};
 
 function Animation( elem, opts ){
 	if( !( elem && opts ) ){
 		throw new Error('Not all options have been provided!');
-	} 
-	this.elem = elem;
+	}
 	
-	this.init( opts );
+	this[0] = elem;
+	this.length = 1;
 	
-	return this;
+	return init.call( this, opts );
 }
 
 Animation.prototype = { 
-	init : function( opts ){           
-
-		this.interval = Math.round(1000/ ( opts.frameRate || FRAME_RATE ) );
-		this.frames = Math.round( ( opts.duration || 600 ) * ( ( opts.frameRate || FRAME_RATE ) / 1000 ) );
-		
-		// check for supported css animated features and prep for animation
-		for( var key in opts.setStyle ){
-			
-			var opacity = key === 'opacity';
-			
-			this.appendString = opacity ? '' : 'px';
-			
-			if( ( opacity && opts.setStyle[ key ] >= 0 && opts.setStyle[ key ] <= 1 ) || TEST_TYPE.test( key ) ){
-				var start = ( curCSS( elem, key ) + '' || '0').replace('px','') * 1;                                       
-				this.start[ key ] = start;				
-				this.delta[ key ] = ( (opts.setStyle[ key ] + '' || '0').replace('px','') * 1 ) - start;
-			}
-		}
-		
-		if( opts.manualStart !== true ){
-			this.start();
-		}
-		
-		this.init = function(){ return this; };
-		
-		return this;
-	},
 	start : function( frame ){
 		
-		this.frame = frame || 0;
+		this._frame = typeof frame === 'number' ? frame : 0;  
+		var that = this;
 		
-		this.intervalId = setInterval( function(){      
+		this._intervalId = setInterval( function(){      
 			
-			this.advance();
+			that.advance();
 
-			if ( this.frame > this.frames ) {  
-                  	this.stop();
+			if ( that._frame > that._frames ) {  
+				that.stop();
 			}
-		}, this.interval );
+		}, this._interval ); 
+		
+		return this;
 	}, 
 	stop : function(){
-		clearInterval( this.intervalId );                                                      
+		clearInterval( this._intervalId );                                                      
 		
-		if( typeof this.callback === 'function' ){ 
-			this.callback.call( this.elem ); 
+		if( typeof this._callback === 'function' ){ 
+			this._callback.call( this[0] ); 
 		}      
 
 		return this; 
-	},  
+	}, 
+	reverse : function(){
+		for( var key in this._start ){  
+			
+			var start = this._start[ key ];
+
+			this._start[ key ] = start + this._delta[ key ];
+			this._delta[ key ] = this._start[ key ] - start;
+		}
+		if( this._autoStart ){ this.start(); }
+		
+		return this;
+	},
 	advance : function(){
 		
-		for( var key in this.start ){  
-			this.elem.style[ key ] = ((this.frame /= this.frames / 2) < 1) ? this.delta[ key ] / 2 * this.frame * this.frame + this.start[ key ] : -this.delta[ key ] / 2 * ((--this.frame) * (this.frame - 2) - 1) + this.start[ key ] + this.appendString;
+		for( var key in this._start ){
+			this[0].style[ key ] = tween( this._frame, this._frames, this._start[ key ], this._delta[ key ] );
 		}
 		
-		this.frame++;
+		this._frame++;       
+		return this;
 	}
-};    
+};
 
-function advanceSingle(){
-   this.elem.style[ this.key ] = ((this.frame /= this.frames / 2) < 1) ? this.delta[ this.key ] / 2 * this.frame * this.frame + this.start[ key ] : -this.delta[ key ] / 2 * ((--this.frame) * (this.frame - 2) - 1) + this.start[ this.key ] + this.appendString;
-}
-	
+// function testForSimpleCSS( simple, css ){    
+// 	var length = 0;
+// 	
+// 	if( Simples.isObject( css ) ){
+// 		for( var key in css ){
+// 			length++;
+// 		}
+// 	} else if ( typeof css === 'string' ){
+// 		length = 1;
+// 	}
+// 	
+// 	return ( length === 1 );
+// }
+
 Simples.merge(Simples, {
-    animationDefaults: function(){
-	  	
-	},
-    animate: function(){     
-		
-		return testForSimple ? new SimpleAnimation() : new Animation();
+    animationDefaults: function( opts ){
+	  	opts = opts || {};
+		FRAME_RATE = opts.frameRate || FRAME_RATE;
+	}
+});
+
+Simples.extend({
+    animate: function( opts ){    
+		return this.each(function(){     
+			return new Animation( this, opts );
+		});                                     
 	}
 });	
