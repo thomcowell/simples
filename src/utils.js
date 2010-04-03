ea6260c891f69ebc2f7d7389e661f9c290f2a5b2
Tom Cowell <tc@eightsquarestudio.com>
2010-04-03 14:13:48 +0100

    adding hasOwnProperty, might not need

--- src/utils.js
+++ src/utils.js
@@ -12,5 +12,10 @@ Simples.merge( Simples, {
 	isObject : function( obj ){
 		if( !obj ){ return false; }
 		return ( toString.call( obj ) === '[object Object]' );
+	},
+	// Do I need this?
+	hasOwnProperty : function( obj, key ){
+		if( !obj || ! key ){ return false; }
+		return hasOwnProperty.call( obj, key );
 	}
 });
