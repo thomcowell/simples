a60d49f2342ff267181d096e3eab81c1e1ebe5ea
Tom Cowell <tc@eightsquarestudio.com>
2010-07-09 16:56:06 +0100

    trying a dry approach for dom

--- src/dom.js
+++ src/dom.js
@@ -1,28 +1,77 @@
 var STRIP_TAB_NEW_LINE = /[\n\t]/g,
 	LAST_SPACE_OPTIONAL = /\s?$/,
 	FIRST_LAST_SPACES = /^\s?|\s?$/g;
-	
-SimplesElement = {
-	domManip : function( node, fnName, children, posElem, posName, reverse ){
-		options = options || {};
-		if( options.fnName !== undefined ){			
-			if( typeof children === "string" ){
-				var div = document.createElement('div'), position = options.position, positionName = options.positionName, insertName = options.insertName;
-				div.innerHTML = children;
-				children = div.childNodes;
 
-				for(var i=children.length;i>=0;i--){
-					node[ options.fnName ]( children[i], position && positionName ? position[ positionName ] : position );
-				}
-			} else if ( children instanceof Simples ){
-				var i = children.length, position = options.position, positionName = options.positionName, insertName = options.insertName;
-				while(i>-1){
-					node[ options.fnName ]( children[i], position && positionName ? position[ positionName ] : position );
-					i--;
-				}			
-			} else if ( children && children.nodeType === 1 ){
-				node[ options.fnName ]( children, options.position && options.positionName ? options.position[ options.positionName ] : options.position );
+function setUpOptions( elem, type, content ){
+	switch( type ){
+		case 'append':
+			return {
+				node : elem,
+				fnName : 'appendChild'
+			};
+		case 'prepend':
+			return {
+				node : elem,
+				fnName : 'insertBefore',
+				position : elem.firstChild
+			};
+		case 'after':
+			return {
+				node : elem.parentNode,
+				fnName : 'insertBefore',
+				position : elem.nextSibling
+			};
+		case 'before':
+			return {
+				node : elem.parentNode,
+				fnName : 'insertBefore',
+				reverse : true,
+				position : elem
+			};   
+		case 'wrap':
+			return {
+				node : elem.parentNode,
+				fnName : 'insertBefore',
+				position : elem
+			};			
+		case 'replaceWith':
+			cleanData( elem );
+			return {
+				node : elem.parentNode,
+				fnName : 'replaceChild',
+				position : elem
 			}
+	};
+	return {};
+};
+
+Simples.extend({
+	insert : function( type, children ){
+		var o = setUpOptions( this[0], type ), fnName = o.fnName;
+		
+		if( o.fnName && typeof children === "string" ){
+			this.each(function(){
+				
+				var o = setUpOptions( this, type ),
+					div = document.createElement('div');
+                
+				if( o.node ){
+					div.innerHTML = children;
+					children = div.childNodes;
+
+	               	for(var i=children.length;i>=0;i--){
+						o.node[ o.fnName ]( children[i], o.position );
+					}
+				}
+			});
+		} else if ( o.fnName && children instanceof Simples ){
+			var i=0,l=children.length;
+			while(l>-1){
+				o.node[ o.fnName ]( children[i], o.position );
+				l--;
+			}			
+		} else if ( o.fnName && children && children.nodeType === 1 ){
+			o.node[ o.fnName ]( children, o.position );
 		}
 	},
 	html : function( content ){
@@ -31,177 +80,32 @@ SimplesElement = {
 				this.innerHTML = content;
 			});
 		} else {
-						
-			this.empty();
-			this.domManip( this, 'appendChild', content );
+			this.empty().insert( 'append', content );
 		}
 		return this;
 	},
-	append : function( child ){ 
-		this.domManip( this, 'appendChild', child );
-		return this;
-	},  
-	
+	append : function( child ){
+		return this.insert( 'append', child );
+	},
 	prepend : function( child ){
-		domManip( this, 'insertBefore', child, this, 'firstChild' );
-		return this;
+		return this.insert( 'prepend', child );
 	},  
 	after : function( child ){
-		domManip( this.parentNode, 'insertBefore', child, this, 'nextSibling' );
-		return this   	                                                                             
+		return this.insert( 'after', child );
 	},
 	before : function( child ){
-		domManip( this.parentNode, 'insertBefore', child, this ); 
-		// var parent = this.parentNode;
-		// if( parent && typeof child === "string" ){
-		// 	var div = document.createElement('div')
-		// 	div.innerHTML = child;
-		// 	child = div.childNodes;
-		// 	
-		// 	for(var i=0,l=child.length;i<l;i++){  
-		// 		parent.insertBefore( sibling, elem );
-		// 		parent.insertBefore( child[i], this );
-		// 	}
-		// } else if ( parent && child && child.nodeType === 1 ){
-		// 	parent.insertBefore( child, this );
-		// }		                                                                             
+		return this.insert( 'before', child ); 	                                                                             
 	},
 	replaceWith : function( child ){
-		var that = this;
+		return this.insert( 'replaceWith', child );
+	},  
+	clone : function( deep ){
+		var results = new Simples();
 		this.each(function(i){
-			var parent = this.parentNode;
-			if ( parent && element.nodeType ) {    
-				cleanData( this );
-				parent.replaceChild( element, this );
-			}
-			that[i] = element;
-		});                      
-	},
-	hasClass : function( className ){
-		this.nodeType ? ( (" " + this.className + " ").replace(STRIP_TAB_NEW_LINE, " ").indexOf( " " + className + " " ) > -1 ) : false		
-	}
-};
-
-SimplesElements = {
-	empty : function(){
-		// Remove element nodes and prevent memory leaks
-		if ( this.nodeType === 1 ) {
-			cleanData( this.getElementsByTagName("*") );
-		}
-
-		// Remove any remaining nodes
-		while ( this.firstChild ) {
-			this.removeChild( this.firstChild );
-		}		
-	},
-	remove : function(){
-		if ( this.parentNode ) { 
-			if ( this.nodeType === 1 ) {
-				cleanData( this.getElementsByTagName("*") );
-			}
-			
-			this.parentNode.removeChild( this );
-		}
-	}
-}
-Simples.extend({ 
-	// html : function( content ){
-	// 		
-	// 	if( content.nodeType || content instanceof Simples ){
-	// 		
-	// 		this.empty();
-	// 		this.appendChild( content );
-	// 	} else if( typeof content === 'string' ){
-	// 		
-	// 		this.each(function(){
-	// 			this.innerHTML = content;
-	// 		});
-	// 	}
-	// 	
-	// 	return this;
-	// },          
-	// appendChild : function( child ){                        
-	//         var elem = this[0];                                            
-	// 	if( typeof child === "string" ){
-	// 		var div = document.createElement('div')
-	// 		div.innerHTML = child;
-	// 		child = div.childNodes;
-	// 		
-	// 		for(var i=0,l=child.length;i<l;i++){
-	// 			elem.appendChild( child[i] );
-	// 		}
-	// 	} else if ( child instanceof Simples ){
-	// 		for(var i=0,l=child.length;i<l;i++){
-	// 			elem.appendChild( child[i] );
-	// 		}
-	// 	} else if ( child && child.nodeType === 1 ){
-	// 		elem.appendChild( child );
-	// 	}            	
-	// 	return this;
-	// },          
-	// prependChild : function( child ){
-	// 	var elem = this[0];
-	// if( typeof child === "string" ){
-	// 	var div = document.createElement('div')
-	// 	div.innerHTML = child;
-	// 	child = div.childNodes;
-	// 	
-	// 	for(var i=child.length;i>=0;i--){
-	// 		elem.insertBefore( child[i], elem.firstChild );
-	// 	}
-	// } else if ( child instanceof Simples ){
-	// 	var i = child.length;
-	// 	while(i>-1){
-	// 		elem.insertBefore( child[i], elem.firstChild );
-	// 		i--;
-	// 	}			
-	// } else if ( child && child.nodeType === 1 ){
-	// 	elem.insertBefore( child, elem.firstChild );
-	// }
-	// 	return this;
-	// },    
-	// before : function( sibling ){
-	// 	var elem = this[0];
-	// 	if ( elem ){
-	// 		var parent = elem.parentNode; 
-	// 		if( sibling.nodeType && parent ){
-	// 			parent.insertBefore( sibling, elem );   
-	// 		} else if( siblings instanceof Simples && parent ){
-	// 			sibling.each(function(){
-	// 				parent.insertBefore( this, elem ); 						
-	// 			});
-	// 		}
-	// 	}
-	// 	return this;
-	// },
-	// after : function( sibling ){
-	// 	var elem = this[0]; 
-	// 	if ( elem ){
-	// 		var parent = elem.parentNode;
-	// 		if( sibling.nodeType && parent ){
-	// 			parent.insertBefore( sibling, elem.nextSibling );   
-	// 		} else if( siblings instanceof Simples && parent ){
-	// 			sibling.each(function(){
-	// 				parent.insertBefore( this, elem.nextSibling ); 						
-	// 			});
-	// 		}
-	// 	}
-	// 	return this;
-	// },      
-	cloneNode : function( deep ){
-		return this[0].cloneNode( deep );
-	},
-	replaceWith : function( child ){
-		SimplesElement.insert( this.parentNode, child, 'replaceChild', this ); 
-		// var that = this;
-		// this.each(function(i){
-		// 	var parent = this.parentNode;
-		// 	if ( parent && element.nodeType ) {    
-		// 		cleanData( this );
-		// 		parent.replaceChild( element, this );
-		// 	}
-		// 	that[i] = element;
-		// });   
+			results[i] = this.cloneNode( deep );
+			results.length = i+1;
+		});
+		return results;
 	},
 	wrap : function( selector ) {
 		var tag, elem; 
@@ -231,7 +135,7 @@ Simples.extend({
     },
 	unwrap : function(){
 		if( this.length ){
-			results = [];
+			results = new Simples();
 			this.each(function(){
 				var parent = this.parentNode;
 				if( parent ){
@@ -250,10 +154,9 @@ Simples.extend({
 	},
 	empty : function(){             
 		this.each(function(){
-			// Remove element nodes and prevent memory leaks
-			if ( this.nodeType === 1 ) {
-				cleanData( this.getElementsByTagName("*") );
-			}
+			
+			
+				cleanElemData( this );
 
 			// Remove any remaining nodes
 			while ( this.firstChild ) {
@@ -275,12 +178,12 @@ Simples.extend({
 	// attributes	
 	hasClass : function( className ){
 		className = " " + className + " ";
-		// for ( var i = 0, l = this.length; i < l; i++ ) {
-		// 	if ( (" " + this[0].className + " ").replace(STRIP_TAB_NEW_LINE, " ").indexOf( className ) > -1 ) {
-		// 		return true;
-		// 	}
-		// }             
-		return this[0] ? ( (" " + this[0].className + " ").replace(STRIP_TAB_NEW_LINE, " ").indexOf( className ) > -1 ) : false;
+		for ( var i = 0, l = this.length; i < l; i++ ) {
+			if ( (" " + this[i].className + " ").replace(STRIP_TAB_NEW_LINE, " ").indexOf( className ) > -1 ) {
+				return true;
+			}
+		}
+		return false;
 	},     
 	addClass : function( className ){
 		this.each(function(){
