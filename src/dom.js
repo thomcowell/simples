17036d33eb0306660c7c64d8bef518774cba0bc5
Tom Cowell <tc@eightsquarestudio.com>
2010-07-20 13:07:52 +0100

    latest dom changes, removing extraneous functions and simplifying into html

--- src/dom.js
+++ src/dom.js
@@ -1,107 +1,107 @@
-var STRIP_TAB_NEW_LINE = /[\n\t]/g,
+var STRIP_TAB_NEW_LINE = /\n|\t/g,
 	LAST_SPACE_OPTIONAL = /\s?$/,
 	FIRST_SPACE = /^\s+/,
+	IMMUTABLE_ATTR = /(button|input)/i,
+	SPECIAL_URL = /href|src|style/,
 	FIRST_LAST_SPACES = /^\s?|\s?$/g;
 
+// Borrowed from XUI project
+// private method for finding a dom element
+function getTag(el) {
+    return (el.firstChild === null) ? {'UL':'LI','DL':'DT','TR':'TD'}[el.tagName] || el.tagName : el.firstChild.tagName;
+}
+
+function wrapHelper(html, el) {
+  return (typeof html == "string") ? wrap(html, getTag(el)) : html && html.each ? slice.call( html, 0 ) : html;
+}
+
+// private method
+// Wraps the HTML in a TAG, Tag is optional
+// If the html starts with a Tag, it will wrap the context in that tag.
+function wrap(xhtml, tag) {
+
+    var attributes = {},
+        re = /^<([A-Z][A-Z0-9]*)([^>]*)>(.*)<\/\1>/i,
+        element,
+        x,
+        a,
+        i = 0,
+        attr,
+        node,
+        attrList;
+
+    if (re.test(xhtml)) {
+        result = re.exec(xhtml);
+        tag = result[1];
+
+        // if the node has any attributes, convert to object
+        if (result[2] !== "") {
+            attrList = result[2].split(/([A-Z]*\s*=\s*['|"][A-Z0-9:;#\s]*['|"])/i);
+
+            for (; i < attrList.length; i++) {
+                attr = attrList[i].replace(/^\s*|\s*$/g, "");
+                if (attr !== "" && attr !== " ") {
+                    node = attr.split('=');
+                    attributes[node[0]] = node[1].replace(/(["']?)/g, '');
+                }
+            }
+        }
+        xhtml = result[3];
+    }
+
+    element = document.createElement(tag);
+    
+	Simples( element ).attr( attributes );
+
+    element.innerHTML = xhtml;
+    return element;
+}
+
+function clean( el ){
+	cleanData( el ); 
+	Simples( el ).empty();
+}
+
 Simples.extend({
-	domManip : function( children, reverse, insertFn ){
-        if( !isFunction( insertFn ) ){ return this; }
-		if( typeof children === "string" ){
-			this.each(function(){
-				
-				var div = document.createElement('div');
+	html : function( location, html ){
+		if (arguments.length === 0) {
+           return this[0].innerHTML;
+       }
 
-				div.innerHTML = children;
-				children = div.childNodes;
+       if ( arguments.length === 1 && arguments[0] !== 'remove') {
+           html = location;
+           location = 'inner';
+       }
 
-               	for(var i=children.length;i>=0;i--){
-					insertFn.call( this, children[i] );
-				}
-			});
-		} else if ( children instanceof Simples ){
-			var i=0,l=children.length;
-			while(l){
-				l--;
-				insertFn.call( this, children[l] );
-			}			
-		} else if ( children && children.nodeType === 1 ){
-			insertFn.call( this[0], children[i] );
-		}
+       return this.each(function() {
+           var el = this, parent =this.parentNode, list, len, i = 0;
+           if (location == "inner") {  
+               if (typeof html === "string") {
+                   el.innerHTML = html;
+                   list = el.getElementsByTagName('SCRIPT');
+                   len = list.length;
+                   for (; i < len; i++) {
+                       eval(list[i].text);
+                   }
+               } else {
+                   el.innerHTML = '';
+                   el.appendChild( html && html.each ? slice.call( html, 0 ) : html);
+               }
+           } else if (location == "outer") {
+               parent.replaceChild( wrapHelper(html, el), el);
+           } else if (location == "top") {
+               el.insertBefore( wrapHelper(html, el), el.firstChild);
+           } else if (location == "bottom") {
+               el.insertBefore( wrapHelper(html, el), null);
+           } else if (location == "remove") {
+               parent.removeChild(el);
+           } else if (location == "before") {
+               parent.insertBefore( wrapHelper(html, parent), el);
+           } else if (location == "after") {
+               parent.insertBefore( wrapHelper(html, parent), el.nextSibling);
+		   }
+        });
 		return this;
-	},
-	html : function( content ){
-		if( typeof content === 'string' ){
-			this.each(function(){
-				this.innerHTML = content;
-			});
-		} else {
-			this.empty().append( content );
-		}
-		return this;
-	},
-	append : function( value ){
-		return this.domManip( value, false, function( node ){
-			if ( this.nodeType === 1 ) {
-				this.appendChild( node );
-			}
-		});
-	},
-	prepend : function( value ){
-		return this.domManip( value, true, function( node ){
-			if ( this.nodeType === 1 ) {
-				this.insertBefore( node, this.firstChild );
-			}
-		});
-	},  
-	after : function( value ){
-		return this.domManip( value, true, function( node ){
-			if ( this && this.parentNode ) {
-				this.parentNode.insertBefore( node, this.nextSibling );
-			}
-		});
-	},
-	before : function( value ){
-		return this.domManip( value, false, function( node ){
-			if ( this && this.parentNode ) {
-				this.parentNode.insertBefore( node, this );
-			}
-		});
-	},
-	replaceWith : function( value ){
-		this.each(function(){
-			var next = this.nextSibling, parent = this.parentNode;
-			cleanData( this );
-			parent.removeChild( this );
-			if( next ){
-				Simples( next ).before( value );
-			} else {
-				Simples( parent ).append( value );
-			}
-		});
-	},  
-	clone : function(){
-		var results = [];
-		var noCloneEvent = Simples.support.noCloneEvent;
-		this.each(function(i){
-			if( noCloneEvent ){
-				var html = this.outerHTML, ownerDocument = this.ownerDocument;
-
-				if ( !html ) {
-					var div = ownerDocument.createElement("div");
-					div.appendChild( this.cloneNode(true) );
-					html = div.innerHTML;
-				}
-
-				var elem = ownerDocument.createElement("div");
-				elem.innerHTML = html;
-				cleanData( elem );
-
-				results.concat( slice.call( elem.childNodes ) );
-			} else {
-				results.push( this.cloneNode( true ) );
-			}
-		});
-		return Simples( results );
 	},
 	wrap : function( selector ) {		
 		var tag, domElem;
@@ -128,29 +128,25 @@ Simples.extend({
 		return this;
     },
 	unwrap : function(){
-		if( this.length ){
-			results = new Simples();
-			this.each(function(){
-				var parent = this.parentNode;
-				if( parent ){
-					var children = this.childNodes;
-					for(var i=0,l=children.length;i<l;i++){
-
-						parent.insertBefore( children[i], this );
-					}
-					this.remove();
-					results.concat( children );
-				}
-			});
-			return Simples( results );
-		}
-		return this;
-	},
-	empty : function(){             
+		if( !this.length ){ return this; }
+		results = [];
 		this.each(function(){
-			if ( this.nodeType === 1 ) {
-				cleanData( this, false );
+			var parent = this.parentNode;
+			if( parent ){
+				var children = slice.call( this.childNodes, 0);
+				for(var i=0,l=children.length;i<l;i++){
+
+					parent.insertBefore( children[i], this );
+				}
+				Simples(this).remove();
+				results.concat( children );
 			}
+		});
+		return Simples( results );
+	},
+	empty : function(){            
+		this.each(function(){
+			cleanData( this, false );
 			// Remove any remaining nodes
 			while ( this.firstChild ) {
 				this.removeChild( this.firstChild );
@@ -160,10 +156,7 @@ Simples.extend({
 	remove : function(){
 		this.each(function(){
 			if ( this.parentNode ) { 
-				if ( this.nodeType === 1 ) {
-					cleanData( this );
-				}
-				
+				cleanData( this );				
 				this.parentNode.removeChild( this );
 			}
 		});
@@ -181,28 +174,62 @@ Simples.extend({
 	addClass : function( className ){
 		this.each(function(){
 			if( !Simples(this).hasClass( className ) ){
-				this.className = ( this.className.replace( STRIP_TAB_NEW_LINE, " ").replace( LAST_SPACE_OPTIONAL, '') + ' '+className ).replace( FIRST_SPACE,'');
+				classname = ( this.className.replace( STRIP_TAB_NEW_LINE, " ").replace( LAST_SPACE_OPTIONAL, '') + ' '+className );
+				this.className = classname.replace( FIRST_SPACE,'');
 			} 
 		});
 		return this;
 	},
 	removeClass : function( className ){
 		this.each(function(){
-			className = (' ' + this.className.replace( STRIP_TAB_NEW_LINE, ' ' ) +' ').replace( ' '+className+' ', ' ' );
+			className = (' ' + this.className.replace( STRIP_TAB_NEW_LINE, " ") +' ').replace( ' '+className+' ', ' ' );
 			this.className = className.replace( FIRST_LAST_SPACES, '' );
 		});
 		return this;		
 	},
 	attr : function(name, value){
-		if( name && value ){
-			this.each(function(){
-				if ( this.nodeType === 1 ) { 
-					this.setAttribute(name, value);
+		var special = SPECIAL_URL.test( name ),
+			nameClass = toString.call( name );
+			
+		if( nameClass === ObjectClass ){
+			for( var key in name ){
+				this.attr( key, name[key] );
+			}
+		} else if( nameClass === StringClass && value ){
+			this.each(function(){                                  
+				if ( this.nodeType === 3 || this.nodeType === 8 || ( name === "type" && IMMUTABLE_ATTR.test( this.nodeName ) ) ) {
+					return undefined;
+				}
+				if ( this.nodeType === 1 && !special && name in this ) { 
+					this[ name ] = value;
+				} else if( name === "style" && !Simples.support.style ){
+					elem.style.cssText = "" + value;
+				} else {
+					this.setAttribute(name, ""+value);
 				}
 			});
-		} else if( name && value === undefined ){
-			return this[0] ? this[0].getAttribute( name ) : null;
-		} else if( name && value === null){
+		} else if( nameClass === StringClass && value === undefined ){                
+			var elem;
+			for(var i=0,l=this.length;i<l;i++){
+				if ( this.nodeType !== 3 && this.nodeType !== 8 ) {
+					elem = this[i];
+					break;
+				}
+			}     
+			if( !elem ){ return null; }     
+			// browsers index elements by id/name on forms, give priority to attributes.
+			if ( elem.nodeName.toUpperCase() === "FORM" && elem.getAttributeNode(name) ) {
+				return elem.getAttributeNode( name ).nodeValue;
+			} else if( elem.nodeType === 1 && !special && name in elem ){
+				// These attributes require special treatment				
+				return elem[ name ]; 
+			} else if ( name === "style" && !Simples.support.style ){
+				return elem.style.cssText;
+			} else {
+				return elem.getAttribute( name );
+			}
+			return null;
+		} else if( nameClass === StringClass && value === null){
 			this.each(function(){
 				if ( this.nodeType === 1 ) {
 					this.removeAttribute( name );
@@ -224,6 +251,6 @@ Simples.extend({
 		return Simples( results );
 	},
 	eq : function( i ){
-		return Simples( this[ i ] );
+		return Simples( slice.apply( this, i < 0 ? [ i ] : [+i, i+1]  ) );
 	}
 });
--- test/dom.js
+++ test/dom.js
@@ -21,7 +21,6 @@ test("addClass", 3, function() {
 	function testAddClass( startClass, className, endClass ){
 		div[0].className = startClass;
 		div.addClass( className );
-		console.log( div );
 		equal( div[0].className, endClass, "should have className "+ className );
 	}
 
@@ -50,11 +49,86 @@ test("attr", 4, function() {
 	var div = Simples('<div/>');
 	div[0].id = "testArea";
 	equal( div.attr("id"), "testArea", "should read the attr id as testArea");
+	
 	div.attr("id", 'hampster-city');
-	equal( div[0].id, 'hampster-city', "should set the attr id as hampser-city");
+	equal( div[0].id, 'hampster-city', "should set the attr id as hampster-city");
+	
 	var time = new Date().getTime();
-	div.attr("date", new Date().getTime() );
-	equal( div.attr('date'), time, "should set the attr id as hampser-city");
+	div.attr("date", time );
+	equal( div[0].getAttribute('date'), time, "should have set the attr date");
+	
 	div.attr('date', null );
 	ok( !div[0].date, "should remote the date attribute" )
-});                                	
+}); 
+
+test("attr(String)",function(){
+	// This one sometimes fails randomly ?!
+	equals( Simples('#text1').attr('value'), "Test", 'Check for value attribute' );
+
+	equals( Simples('#text1').attr('value', "Test2").attr('defaultValue'), "Test", 'Check for defaultValue attribute' );
+	equals( Simples('#text1').attr('type'), "text", 'Check for type attribute' );
+	equals( Simples('#radio1').attr('type'), "radio", 'Check for type attribute' );
+	equals( Simples('#check1').attr('type'), "checkbox", 'Check for type attribute' );
+	equals( Simples('#simon1').attr('rel'), "bookmark", 'Check for rel attribute' );
+	equals( Simples('#google').attr('title'), "Google!", 'Check for title attribute' );
+	equals( Simples('#mark').attr('hreflang'), "en", 'Check for hreflang attribute' );
+	equals( Simples('#en').attr('lang'), "en", 'Check for lang attribute' );
+	equals( Simples('#simon').attr('class'), "blog link", 'Check for class attribute' );
+	equals( Simples('#name').attr('name'), "name", 'Check for name attribute' );
+	equals( Simples('#text1').attr('name'), "action", 'Check for name attribute' );
+
+	ok( Simples('#form').attr('action').indexOf("formaction") >= 0, 'Check for action attribute' );
+	// Temporarily disabled. See: #4299
+	// ok( Simples('#form').attr('action','newformaction').attr('action').indexOf("newformaction") >= 0, 'Check that action attribute was changed' );
+	equals( Simples('#text1').attr('maxlength'), '30', 'Check for maxlength attribute' );
+	equals( Simples('#text1').attr('maxLength'), '30', 'Check for maxLength attribute' );
+	equals( Simples('#area1').attr('maxLength'), '30', 'Check for maxLength attribute' );
+	equals( Simples('#select2').attr('selectedIndex'), 3, 'Check for selectedIndex attribute' );
+	equals( Simples('#foo').attr('nodeName').toUpperCase(), 'DIV', 'Check for nodeName attribute' );
+	equals( Simples('#foo').attr('tagName').toUpperCase(), 'DIV', 'Check for tagName attribute' );	
+
+	Simples('#main').html( 'bottom', "<a href='#5' id='tAnchor5'></a>" );
+	equals( Simples('#tAnchor5').attr('href'), "#5", 'Check for non-absolute href (an anchor)' );
+
+	equals( Simples("<option/>").attr("selected"), false, "Check selected attribute on disconnected element." );
+})
+
+test("html",function(){
+	ok( false, "tests not written" );
+});
+
+test("clone",function(){
+	ok( false, "tests not written" );
+});
+
+test("wrap",function(){
+	ok( false, "tests not written" );
+});
+
+test("unwrap",function(){
+	ok( false, "tests not written" );
+});
+
+test("empty", 3, function(){
+	
+	equals( Simples("#ap").empty().html().length, 0, "Check text is removed" );
+	equals( Simples("#ap").length, 4, "Check elements are not removed" );
+
+	// using contents will get comments regular, text, and comment nodes
+	var j = Simples("#nonnodes").traverse('childNodes');
+	j.empty();
+	equals( j.html(), "", "Check node,textnode,comment empty works" );	
+});
+
+test("remove",function(){
+	ok( false, "tests not written" );
+});  
+
+test("fetch",function(){
+	ok( false, "tests not written" );
+});
+
+test("eq",function(){
+	ok( false, "tests not written" );
+});
+// prepend append => 'string' and nodes check order                                	
