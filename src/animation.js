96687b90da94c06c687e277680f7648f8ad86099
Tom Cowell <tc@eightsquarestudio.com>
2010-07-22 14:50:25 +0100

    re-working animation to allow simplified control and running

--- src/animation.js
+++ src/animation.js
@@ -1,4 +1,5 @@
 var FRAME_RATE = 24,
+	GUID = 1e6,
 	TEST_TYPE = /padding|margin|height|width|top|left|right|bottom/,
 	TWEENS = {
 		easing : function( frame, frameCount, start, delta) {
@@ -11,104 +12,141 @@ var FRAME_RATE = 24,
 			return start + (((Math.cos((frame/frameCount)*Math.PI) )/2) * delta );
 		}
 	};   
-	
-function init( opts ){
 
-	this._interval = Math.round(1000/ ( opts.frameRate || FRAME_RATE ) );
-	this._frames = Math.round( ( opts.duration || 600 ) * ( ( opts.frameRate || FRAME_RATE ) / 1000 ) );   
-	this._start = {};
-	this._delta = {};
+var AnimationController = {
+	animations : {},
+	length : 0,
+	timerID : null,
+	cycle : false,    
+	last : 0,
+	interval : Math.round( 1000/ FRAME_RATE ),
+	start : function( animation ){
+		if( !hasOwn.call( this.animations, animation._id ) ){
+			this.length++;
+			this.animations[ animation._id ] = animation;
+		}
+		
+		if( !this.timerID ){
+			this.interval = Math.round( 1000/ FRAME_RATE );
+			this.timerID = window.setInterval( AnimationController.step, this.interval );
+			this.step();
+		}
+	},
+	step : function(){          
+		if( this.length ){     
+			if( this.cycle ){
+				for( var id in this.animations ){
+					this.animations[ id ]._frame++;
+				}
+			} else {
+				this.cycle = true;
+				for( di in this.animations ){
+					this.animations[ di ].step();
+				}
+				this.cycle = false;
+			}			
+		} else if( this.timerID ){
+			window.clearInterval( this.timerID );
+			this.timerID = null;
+		}
+	},
+	stop : function( animation ){
+		if( animation ){
+			if( hasOwn.call( this.animations, animation._id ) ){
+				delete this.animations[ animation._id ];        
+				this.length--;
+			}
+		}
+	}
+};
+
+function Animation( elem, opts ){
+	
+	if( !( elem && elem.nodeType === 1 && opts ) ){
+		throw new Error('Not all options have been provided!');
+	}
+	
+	this[0] = elem; 
+	this.length = 1;
+	this._id = ++GUID;
+	this._callback = ( typeof opts.callback === 'function' ) ? opts.callback : Simples.noop;
+	this._reverse = ( opts.reverse === true ) ? this.reverse : Simples.noop;
+	this._autoStart = opts.manualStart !== true;
+
+	var frames = Math.round( ( opts.duration || 600 ) * ( FRAME_RATE / 1000 ) ), _start = {}, _delta = {}, _end = {};
 	
 	// check for supported css animated features and prep for animation
 	for( var key in opts.setStyle ){
 		
 		var opacity = ( key === 'opacity' && opts.setStyle[ key ] >= 0 && opts.setStyle[ key ] <= 1 );
-		
+
 		if( opacity || TEST_TYPE.test( key ) ){
-
-			this._appendString = opacity ? '' : 'px';
-
-			var start = ( currentCSS( this[0], key ) + '' || '0').replace('px','') * 1;
-
-			this._start[ key ] = start;				
-			this._delta[ key ] = ( (opts.setStyle[ key ] + '' || '0').replace('px','') * 1 ) - start;
+			_start[ key ] = ( this.css( key ) + '' || '0').replace('px','') * 1;
+            _end[ key ] = (opts.setStyle[ key ] + '' || '0').replace('px','') * 1;
+			_delta[ key ] = _end[ key ] - _start[ key ];
 		}                                        
-	}                                            
-	this._tween = opts.tween && TWEENS.propertyIsEnumerable( opts.tween ) ? TWEENS[ opts.tween ] : TWEENS.easing;
-	
-	this._callback = ( typeof opts.callback === 'function' ) ? opts.callback : opts.reverse ? Simples.setContext( this, this.reverse ) : null;
-
-	this._autoStart = opts.manualStart !== true; 
-	
-	return this._autoStart ? this.start() : this;	
-}
-
-function Animation( elem, opts ){
-	if( !( elem && opts ) ){
-		throw new Error('Not all options have been provided!');
 	}
 	
-	this[0] = elem;
-	this.length = 1;
+	var _tween = opts.tween && TWEENS.propertyIsEnumerable( opts.tween ) ? TWEENS[ opts.tween ] : TWEENS.easing;
+	this._frames = [ _start ];
 	
-	return init.call( this, opts );
+	for(var i=0;i<=frames;i++){
+		
+		var css = {};
+		for( var name in _start ){
+			css[ name ] = _tween( i, frames, _start[ name ], _delta[ name ] );
+		}
+		
+		this._frames.push( css );
+	}
+	
+	this._frames.push( _end );
+	
+	return this._autoStart ? this.start() : this;
 }
 
-Animation.prototype = { 
+Animation.prototype = {  
+	css : Simples.prototype.css,
 	start : function( frame ){
-		
-		this._frame = ( typeof frame === 'number' ) ? frame : 0;  
-		var that = this;
-		
-		this._intervalId = setInterval( Simples.setContext( this, this.advance ), this._interval ); 
+		this._frame = ( typeof frame !== 'number' ) ? 0 : ( 0 < frame ? frame : this._frames.length + frame );
+		AnimationController.start( this );
 		
 		return this;
 	}, 
 	stop : function(){
 		
-		clearInterval( this._intervalId );                                                      
-		
-		if( typeof this._callback === 'function' ){ 
-			this._callback.call( this[0] ); 
-		}      
-
+		AnimationController.stop( this );
+		this._callback.call( this );
+		this._reverse.call( this );
 		return this; 
-	}, 
-	reverse : function(){
-		for( var key in this._start ){  
-			
-			var start = this._start[ key ];
-
-			this._start[ key ] = start + this._delta[ key ];
-			this._delta[ key ] = this._start[ key ] - start;
-		}
-		if( this._autoStart ){ this.start(); }
+	},                          
+	reverse : function( start ){
+		this._frames.reverse();
+		if( this._autoStart || start ){ this.start(); }
 		
 		return this;
 	},
-	advance : function(){
+	reset : function(){
+		this.css( this._frames[0] );
 		
-		for( var key in this._start ){
-			this[0].style[ key ] = this._tween( this._frame, this._frames, this._start[ key ], this._delta[ key ] );
-		}
+		return this;
+	},
+	step : function(){                                                                               
 		
+		this.css( this.frames[ this._frame ] );
 		this._frame++;   
 		
-		if ( this._frame > this._frames ) { this.stop(); }
+		if ( this._frame >= this._frames.length ) { this.stop(); }
 		    
 		return this;
 	}
-};  
+}; 
 
-
-function CompositeAnimation( elems, opts ){
-	this.length = 0;
-	var that = this;
+function CompositeAnimation( animations ){
 	
-	elems.each(function(){
-		that[ that.length ] = new Animation( this, opts );
-		that.length++;
-	});
+	if( toString.call(animations) === ArrayClass ){
+		merge.call( this, animations );
+	}
 	
 	return this;
 }
@@ -116,7 +154,9 @@ function CompositeAnimation( elems, opts ){
 for( var key in Animation.prototype ){
 	CompositeAnimation.prototype[ key ] = function(){
 		for(var i=0,l=this.length;i<l;i++){
-			this[ i ][ key ]();
+			if( this[ i ][ key ] ){
+				this[ i ][ key ].apply( this[ i ], arguments );
+			}
 		}
 		return this;
 	};
@@ -125,7 +165,11 @@ for( var key in Animation.prototype ){
 Simples.merge(Simples, {
     animationDefaults: function( opts ){
 	  	opts = opts || {};
-		FRAME_RATE = opts.frameRate || FRAME_RATE;
+		
+		if( !AnimationController.timerID ){
+			FRAME_RATE = opts.frameRate || FRAME_RATE;
+		}
+		
 		if( opts.tweens ){
 			for( var key in opts.tweens ){
 				if( !hasOwn.call( TWEENS, key ) ){
@@ -137,11 +181,13 @@ Simples.merge(Simples, {
 });
 
 Simples.extend({
-    animate: function( opts ){    
-		if( this.length === 1){
-			return new Animation( this[0], opts );
-		} else {
-			return new CompositeAnimation( this, opt );
-		}                                
+    animate: function( opts ){
+		var animations = [];
+		if( opts ){		
+			this.each(function(){
+				animations.push( new Animation( this, opts ) );
+			});
+		}
+		return animations.length > 1 ? new CompositeAnimation( animations ) : animations[0];
 	}
 });	
