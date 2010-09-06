8b1b2d819059526bca398e510c8cd636382234cb
Tom Cowell <tc@eightsquarestudio.com>
2010-09-06 20:32:39 +0100

    refactoring data to have a cleaner interface

--- test/data.js
+++ test/data.js
@@ -13,93 +13,100 @@ test("noData", 6, function() {
 	ok( !canDoData( applet ), "a applet should return false");	
 });
 
-module("Data: addData");
+module("Data: Simples.data( add )");
 var data = {ham:'sandwich',"super":true};
-test("addData to element with no accessId", 3, function() { 
+test("Simples.data to element with no accessId", 3, function() { 
 	ok( /simples\d+/.test(accessID), "accessID is as expected" )
 	var div = document.createElement('div');
-	addData( div, 'test', data);
+	Simples.data( div, 'test', data);
 
 	notEqual( div[ accessID ], null, "a div should have the attribute of "+accessID+" set"); 	
 	same( div[ accessID ][ 'test' ], data, "div should return the same data as provided");   
 });
 
-test("addData to element with existing accessId", 1, function() { 
+test("Simples.data to element with existing accessId", 1, function() { 
 	var div = document.createElement('div');
 	div[ accessID ] = {};
-	addData( div, 'test', data);
+	Simples.data( div, 'test', data);
 
 	same( div[ accessID ][ 'test' ], data, "div should return the same data as provided");   
 });
 
-test("addData to element with existing accessId", 2, function() { 
+test("Simples.data to element with existing accessId", 2, function() { 
 	var string = 'ready steady go';
 	var div = document.createElement('div');
 	div[ accessID ] = {hammer: string};
-	addData( div, 'test', data);
+	Simples.data( div, 'test', data);
 
 	same( div[ accessID ][ 'test' ], data, "div should return the same data as provided");   
 	same( div[ accessID ][ 'hammer' ], string, "div should return the same data as provided");   
 });
 
-test("addData to element with existing accessId", 1, function() { 
+test("Simples.data to element with existing accessId", 1, function() { 
 	var string = 'ready steady go';
 	var div = document.createElement('div');
 	div[ accessID ] = {hammer: string};
-	addData( div, 'hammer', data);
+	Simples.data( div, 'hammer', data);
 
 	same( div[ accessID ][ 'hammer' ], data, "div should return the same data as provided");   
 });
 
-test("addData to element with existing accessId", 1, function() { 
+test("Simples.data to element with existing accessId", 1, function() { 
 	var div = document.createElement('div');
 	div[ accessID ] = {};
-	addData( div, 'test', data);
+	Simples.data( div, 'test', data);
 
 	same( div[ accessID ][ 'test' ], data, "div should return the same data as provided");   
 });
 
-test("addData on invalid element", 1, function() { 
+test("Simples.data on invalid element", 1, function() { 
 	var object = document.createElement('object');
-	addData( object, 'test', data);
+	Simples.data( object, 'test', data);
 
 	equal( object[ accessID ], undefined, "a div should have the attribute of "+accessID+" set"); 	
 });
 
-module("Data: readData");
-test("readData on element without data attribute", 1, function() {
+module("Data: Simples.data( read )");
+test("Simples.data on element without data attribute", 2, function() {
 	var div = document.createElement('div');
-	same( readData( div, 'test'), null, "a div should return undefined as provided");
+	same( Simples.data( div ), {}, "a div should return undefined as provided");
+	same( div[ accessID ], {}, "a div should return undefined as provided");
 });
 
-test("readData on invalid element", 1, function() {
+test("Simples.data on element without data attribute", 2, function() {
+	var div = document.createElement('div');
+	same( Simples.data( div, 'test'), undefined, "a div should return undefined as provided");
+	same( div[ accessID ], {}, "a div should return undefined as provided");
+});
+
+test("Simples.data on invalid element", 1, function() {
 	var object = document.createElement('object');
-	same( readData( object, 'test'), null, "a div should return undefined as provided");
+	same( Simples.data( object, 'test'), null, "a div should return undefined as provided");
 });
 
-test("readData on element with data as requested", 2, function() {
+test("Simples.data on element with data as requested", 2, function() {
 	var r_div = document.createElement('div');
-	r_div[ accessID ] = {test:data};
+	r_div[ accessID ] = { test: data };
 	
-	same( readData( r_div, 'test'), data, "a div should return the same as provided");
-	same( readData( r_div, 'note'), undefined, "a div should return undefined as provided");
+	same( Simples.data( r_div, 'test'), data, "a div should return the same as provided");
+	same( Simples.data( r_div, 'note'), undefined, "a div should return undefined as provided");
 }); 
 
-module("Data: removeData");
-test("removeData on invalid element", 1, function() {
+module("Data: Simples.data( remove )");
+test("Simples.data on invalid element", 1, function() {
 	var object = document.createElement('object');
 	var noError = true;
 	try{
-		removeData( object, 'test');
+		Simples.data( object, 'test', null);
 	}catch(e){ noError = false; }
 	ok( noError, "should not throw an error");
 });
 
-test("removeData on element with data as requested", 1, function() {
+test("Simples.data on element with data as requested", 1, function() {
 	var r_div = document.createElement('div');
 	r_div[ accessID ] = {test:data};
 	
-	removeData( r_div, 'test');	
+	Simples.data( r_div, 'test', null);	
 	same( r_div[ accessID ].test, undefined, "a div should have data removed");
 });
 
@@ -130,15 +137,15 @@ test("cleanData on element with data as requested", 1, function() {
 
 module("Data: Simples instance", {
 	setup: function(){
-		window.old_addData = addData;
-		window.old_removeData = removeData;
-		window.old_readData = readData;
+		window.old_addData = Simples.data;
+		window.old_removeData = Simples.data;
+		window.old_readData = Simples.data;
 		
 	},
 	teardown : function(){
-		addData = window.old_addData;
-		readData = window.old_readData;
-		removeData = window.old_removeData;
+		Simples.data = window.old_addData;
+		Simples.data = window.old_readData;
+		Simples.data = window.old_removeData;
 	}
 });
 
@@ -160,13 +167,13 @@ test("on an simples object with a single element",function(){
 	
 	same(s_obj.data('hammer', null), s_obj, "should return self and shouldn't throw an exception");
 	
-	addData( div, 'hammer', 'thor\'s' );
+	Simples.data( div, 'hammer', 'thor\'s' );
 	same(s_obj.data('hammer', null), s_obj, "should not return anything and shouldn't throw an exception"); 
-	equal( readData(div, 'hammer'), null, "should call the removeData function");
+	equal( Simples.data(div, 'hammer'), null, "should call the Simples.data function");
 	  
 	same(s_obj.data('hammer', 'thor\'s'), s_obj, "should return self and shouldn't throw an exception");	
 	s_obj.data('hammer', 'thor\'s');
-	same('thor\'s', readData( div, 'hammer'), "set the data on the element");
+	same('thor\'s', Simples.data( div, 'hammer'), "set the data on the element");
 	same('thor\'s', s_obj.data('hammer'), "return the data from the element");	
 	
 });
@@ -179,8 +186,8 @@ test("on an simples object with multiple elements",function(){
 	same(s_obj.data('hammer', null), s_obj, "should return self and shouldn't throw an exception");
 	
 	s_obj.each(function(){
-		addData( this, 'hammer', 'thor\'s' );
-		if( !readData( this, 'hammer' ) ){
+		Simples.data( this, 'hammer', 'thor\'s' );
+		if( !Simples.data( this, 'hammer' ) ){
 			ok(false, "Test not correctly setup");
 		}
 	});
@@ -188,18 +195,18 @@ test("on an simples object with multiple elements",function(){
 	same(s_obj.data('hammer', null), s_obj, "should not return anything and shouldn't throw an exception");
 	
 	s_obj.each(function(){
-		equal( readData(this, 'hammer'), null, "should call the removeData function");
+		equal( Simples.data(this, 'hammer'), null, "should call the Simples.data function");
 	});
 	  
 	same(s_obj.data('hammer', 'thor\'s'), s_obj, "should return self and shouldn't throw an exception");
 	
 	s_obj.data('hammer', 'thor\'s');
 	s_obj.each(function(){ 
-		same('thor\'s', readData( this, 'hammer'), "set the same data on all the elements");
+		same('thor\'s', Simples.data( this, 'hammer'), "set the same data on all the elements");
 	});
 	
 	s_obj.data('hammer', null);
-	addData( s_obj[0], 'hammer', 'head');
+	Simples.data( s_obj[0], 'hammer', 'head');
 	same('head', s_obj.data('hammer'), "should only return data from the first element");	
 	
 });
