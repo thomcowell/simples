bd3823c9de62c08fa9cb74dd60b32c1d421d27f0
Thomas Cowell <tcowell@eightsquarestudio.com>
2012-01-06 10:04:42 +0000

    adding in test stubs

---
+++ test/message.js
@@ -0,0 +1,29 @@
+module("messaging");
+// sense check to ensure no browser incorrectly returns bad values
+test("listeners bound", 1, function(){
+
+});
+
+test("single listeners bound", 1, function(){
+
+});
+
+test("stopListening for listeners ", 1, function(){
+
+});
+
+test("stopListening for single listeners ", 1, function(){
+
+});
+
+test("send for listeners ", 1, function(){
+
+});
+
+test("send for single listeners ", 1, function(){
+
+});
+
+test("send for single listeners and listeners", 1, function(){
+
+});
