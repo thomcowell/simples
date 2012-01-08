4dd40441dad2777e38a57377f131fc04d1ce4a8c
Thomas Cowell <tcowell@eightsquarestudio.com>
2012-01-08 22:39:47 +0000

    latest messaging interface -- Needs rewrite --

--- src/message.js
+++ src/message.js
@@ -1,12 +1,18 @@
 Simples.message = (function( undef ){
 	var listeners = {},singleListener ={},listenerFired = {};
+
 	return {
-		listen : function( type, callback, single ){
-			if( !(type && typeof callback === "function") ){ return false; }
-			if( !callback.guid ){ callback.guid = "simples-guid-" + Simples.guid++; }
+		listenFor : function( type, callback, single ){
+			if( !type || typeof callback !== "function") ){ return false; }
+			if( !callback.guid ){ 
+				callback.guid = "simples-guid-" + Simples.guid++;
+			}
 			if( single === true ){
 				if( listenerFired[ type ] ){
-					callback( listenerFired[ type ] );
+					setTimeout(function(){
+						callback( listenerFired[ type ] );
+					},13);
+					return true;
 				} else {
 					singleListener[ type ] = singleListener[ type ] || [];
 					singleListener[ type ].push( callback );
@@ -18,8 +24,10 @@ Simples.message = (function( undef ){
 			return callback.guid;
 		},
 		send : function( type, data ){
-			if( !data ){ return false; }
-			var callbacks = singleListener[ type ] || listeners[ type ] || [],i=0,l=callbacks.length;
+			if( !type ){ return false; }
+			var callbacks = [].push.apply( (listeners[ type ] || []).slice(0), (singleListener[ type ] || []).slice(0) ),
+				i=0,
+				l=callbacks.length;
 			while( i<l ){
 				callbacks[i++]( data );
 			}
@@ -31,24 +39,48 @@ Simples.message = (function( undef ){
 		},
 		stopListeningFor : function( type, callback ){
 			var guid = typeof callback === "function" ? callback.guid : typeof callback === "string" ? callback : undef;
-			if( !guid || !type || !listeners[ type ] ){ return false; }
-			var callbacks = listeners[ type ],
+			if( !guid || !type || !(listeners[ type ] && listeners[ type ].length) ){ return false; }
+			var callbacks = listeners[type],
 				i=0,
 				l=callbacks.length;
 
 			while( i<l ){
 				if( callbacks[i].guid == guid ){
 					callbacks.splice( i, 1 );
-					return true;
 				}
 				i++;
+			}
+			if( singleListener[ type ] ){
+				var singleCallbacks = singleListener[ type ],
+					i = 0,
+					l = singleCallbacks.length;
+				while( i<l ){
+					if( callbacks[i].guid == guid ){
+						singleCallbackssplice( i, 1 );
+						break;
+					}
+					i++;
+				}
 			}
 			return false;
 		},
 		count : function( type ){
 			if( !type ){ return 0; }
-			var l = listeners[ type ] || 0, s = singleListener[ type ] || 0;
-			return l + s;
+			return [].push.apply( (listeners[ type ] || []).slice(0), (singleListener[ type ] || []).slice(0) ).length;
+		},
+		listening : function( type, callback ){
+			var guid = typeof callback === "function" ? callback.guid : typeof callback === "string" ? callback : undef;
+			if( !guid || !type || !listeners[ type ] ){ return false; }
+			var callbacks = [].push.apply( (listeners[ type ] || []).slice(0), (singleListener[ type ] || []).slice(0) ),
+				i=0,
+				l=callbacks.length;
+
+			while( i<l ){
+				if( callbacks[i].guid == guid ){
+					return true;
+				}
+			}
+			return false;
 		}
 	};
 })();
