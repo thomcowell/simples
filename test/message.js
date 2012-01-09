960723333c5fd46502507573fb7dcf30874c43ca
Thomas Cowell <tcowell@eightsquarestudio.com>
2012-01-09 20:03:35 +0000

    adding in tests for single

--- test/message.js
+++ test/message.js
@@ -3,17 +3,20 @@ module("Messaging");
 test("correct api defined", function(){
 	var apis = ["listenFor","send","silence","count","isListening"];
 	expect( apis.length );
+
 	for(var i=0,l=apis.length;i<l;i++){
 		equal( typeof Simples.message[apis[i]], "function", "should have api - Simples.message."+apis[i] );
 	}
 });
 
-test("listeners bound", 7, function(){
+test("listeners bound", 9, function(){
+	// Test that listenFor responds correctly to bad passed in values
+	strictEqual( false, Simples.message.listenFor(), "should return false if no type and callback is specified" );
+	strictEqual( false, Simples.message.listenFor("hammer"), "should return false if no callback is specified" );
+	strictEqual( false, Simples.message.listenFor("hammer","time"), "should return false if no callback is specified" );
+	strictEqual( false, Simples.message.listenFor(undefined,function(){ok(false,"nope shouldn't fire"); }), "should return false if no callback is specified" );
 
-	ok( !Simples.message.listenFor(), "should return false if no type and callback is specified" );
-	ok( !Simples.message.listenFor("hammer"), "should return false if no callback is specified" );
-	ok( !Simples.message.listenFor(undefined,function(){ok(false,"nope shouldn't fire")}), "should return false if no callback is specified" );
-
+	// Test that listenFor queues up callback correctly
 	var guid = Simples.message.listenFor("data-drum",function( data ){
 		ok( true, "should have bound listener");
 	});
@@ -21,17 +24,36 @@ test("listeners bound", 7, function(){
 	equal( Simples.message.count("data-drum"), 1, "Should have 1 bound callback" );
 	ok( Simples.message.isListening("data-drum", guid), "Should have 1 bound callback" );
 
-	var testFunction = function( data ){
+	// // Test callback with guid is handled correctly
+	function testFunction( data ){
 		ok( true, "should have bound listener");
-	};
-	testFunction.guid = "my-special-guid-12932"
+	}
+	testFunction.guid = "my-special-guid-12932";
 	var guid2 = Simples.message.listenFor("data-drum", testFunction );
 
 	equal( guid2, "my-special-guid-12932", "should not alter existing guid" );
+ 	ok( Simples.message.isListening("data-drum", testFunction), "Should have 1 bound callback" );
 });
 
-test("single listeners bound", 1, function(){
+test("single listeners bound", 5, function(){
+	debugger
+	// Test that listenFor queues up callback correctly
+	var guid = Simples.message.listenFor("data-drum-singles",function( data ){
+		ok( true, "should have bound listener");
+	}, true);
+	equal( guid, "simples-guid-" + ( Simples.guid - 1 ), "should have guid returned" );
+	equal( Simples.message.count("data-drum-singles"), 1, "Should have 1 bound callback" );
+	ok( Simples.message.isListening("data-drum-singles", guid), "Should have 1 bound callback" );
 
+	// Test callback with guid is handled correctly
+	function testFunction( data ){
+		ok( true, "should have bound listener");
+	}
+	testFunction.guid = "my-special-guid-12932";
+	var guid2 = Simples.message.listenFor("data-drum-singles", testFunction, true );
+
+	equal( guid2, "my-special-guid-12932", "should not alter existing guid" );
+	ok( Simples.message.isListening("data-drum-singles", testFunction ), "Should have 1 bound callback" );
 });
 
 test("stopListening for listeners ", 1, function(){
